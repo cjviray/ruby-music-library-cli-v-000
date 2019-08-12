8ee204fb09f4e2374145d76077b58e14e2eeddf7
Christian V <viray.christianj@gmail.com>
2019-08-12 00:07:13 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,22 +22,19 @@ class MusicLibraryController
     
     case input 
       
-      when 'list songs'
-        list_songs
-      when 'list artists'
-        list_artists
-      when 'list genres'
-        list_genres
-      when 'list artist'
-        list_songs_by_artist
-      when 'list genre'
-        list_songs_by_genre
-      when 'play song'
+    when 'list songs'
+      list_songs
+    when 'list artists'
+      list_artists
+    when 'list genres'
+      list_genres
+    when 'list artist'
+      list_songs_by_artist
+    when 'list genre'        
+      list_songs_by_genre
+    when 'play son'
         play_song
       end 
-      
-      
-      
    end 
   end 
   
